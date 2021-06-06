c86dc1955ceef7f269fa862b7b982dc125ec673f
yas93 <yahddak@outlook.fr>
2021-06-06 23:40:13 +0200

    parties inutiles de la classe Joueur

--- Joueur.java
+++ Joueur.java
@@ -2,19 +2,11 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Joueur {
-    
-    private String nom_coup;
     private boolean isWhite;
     private ArrayList<Piece> listepieces; 
 
     public Joueur(boolean iswhite){
         this.isWhite = iswhite;
-    }
-
-    public String SaisirCoup(){
-        Scanner nom_coup = new Scanner();
-        nom_coup.nextString();
-        return nom_coup;
     }
     
     public boolean GetIsWhite(){
